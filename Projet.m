df40327bb37ccd855443a2008a25dfd097779e5f
King Claudy <clocloranely@hotmail.fr>
2017-10-27 09:52:04 +0200

    Première version pour Octave

--- Projet.m
+++ Projet.m
@@ -1,7 +1,10 @@
 for m = 0:1:2
     for a = [0 10 100 1000]
-        f = @(x,y) x*sqrt(1-y)-m*pi-atan(sqrt(y/(1-y)))-atan(sqrt((a+y)/(1-y)));
-        contourf(f, [0 15 0 1], 'MeshDensity',200);
+        % TE - mode
+        TE = @(x,y) x*sqrt(1-y)-m*pi-atan(sqrt(y/(1-y)))-atan(sqrt((a+y)/(1-y)));
+        f1 = contourc(TE(0:15,0:1), [0 15 0 1], 'MeshDensity',200);
+        TM = @(x,y) x*sqrt(1-y)-m*pi-atan(sqrt(y/(1-y)))-atan(sqrt((a+y)/(1-y)));
+        f2 = contourc(TM, [0 15 0 1], 'MeshDensity',200);
         hold on
     end
 end
