8ac04ee136aef61150134ddf4f7c00e1ccd8c2ce
King Claudy <clocloranely@hotmail.fr>
2017-10-27 13:18:18 +0200

    Version quasi-finale du projet

---
+++ Calcul_nombremodes.m
@@ -0,0 +1,5 @@
+function [ M ] = Calcul_nombremodes(l, d)
+
+M = (pi/4)*(2*d/l)^2
+
+end
---
+++ caracteristique_guide.m
@@ -0,0 +1,7 @@
+% Number of modes
+m = 2*Calcul_nombremode(d,l);
+% Effective index for modes
+%% TE mode
+
+%% TM mode
+
