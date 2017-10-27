d581d0f232663889495493f4241cc36529ab439c
King Claudy <clocloranely@hotmail.fr>
2017-10-27 10:44:40 +0200

    Première version fonctionnelle pour Octave

--- Projet.m
+++ Projet.m
@@ -1,10 +1,22 @@
 for m = 0:1:2
     for a = [0 10 100 1000]
         % TE - mode
-        TE = @(x,y) x*sqrt(1-y)-m*pi-atan(sqrt(y/(1-y)))-atan(sqrt((a+y)/(1-y)));
-        f1 = contourc(TE(0:15,0:1), [0 15 0 1], 'MeshDensity',200);
-        TM = @(x,y) x*sqrt(1-y)-m*pi-atan(sqrt(y/(1-y)))-atan(sqrt((a+y)/(1-y)));
-        f2 = contourc(TM, [0 15 0 1], 'MeshDensity',200);
+        [x,y] = meshgrid(linspace(0,15,200),linspace(0,1,200));
+        TE = x.*sqrt(1-y)-m.*pi-atan(sqrt(y./(1-y)))-atan(sqrt((a+y)./(1-y)));
+        f1 = contour(x,y,TE, [0,0]);
+        hold on
+    end
+end
+hold off
+grid on;
+
+figure;
+for m = 0:1:2
+    for a = [0 10 100 1000]
+        % TM - mode
+        [x,y] = meshgrid(linspace(0,15,200),linspace(0,1,200));
+        %TM = @(x,y) x*sqrt(1-y)-m*pi-atan(sqrt(y/(1-y)))-atan(sqrt((a+y)/(1-y)));
+        f1 = contour(x,y,TM, [0,0]);
         hold on
     end
 end
